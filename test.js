e1d496679a7a89b32f4e92c7bd3456ec71dbd1e6
David Momenso <momenso@gmail.com>
2014-08-17 14:19:28 +0000

    sensor pooling

--- test.js
+++ test.js
@@ -6,17 +6,30 @@ var BMP085 = require('bmp085'),
     }),
     dht_sensor = require("node-dht-sensor");
 
-barometer.read(function (data) {
-  console.log("Temperature B : "+data.temperature.toFixed(1)+"C");
-  console.log("Pressure      : "+data.pressure.toFixed(1)+" mbar");
-  
-  var altitude = 44330.0 * (1.0 - Math.pow(data.pressure / 1013.25, (1.0/5.255)));
-  console.log("Altitude      : "+altitude.toFixed(1)+" m");
-  
-  var mslp = data.pressure / Math.pow(1.0 - altitude/44330.0, 5.255);
-  console.log("MSLP          : "+mslp.toFixed(1)+" mbar");  
-});
+var station = {
 
-var readout = dht_sensor.readSpec(22, 4);
-console.log("Temperature A : "+readout.temperature.toFixed(1)+"C");
-console.log("Humidity      : "+readout.humidity.toFixed(1)+"%");
+  start: function() {
+    station.read();
+
+    setTimeout(function() {
+      station.start();
+    }, 5000);
+  },
+  
+  read: function() {
+    barometer.read(function (data) {
+  
+      var readout = dht_sensor.readSpec(22, 4);
+      //var altitude = 44330.0 * (1.0 - Math.pow(data.pressure / 1013.25, (1.0/5.255)));
+      var altitude = 756;
+      var mslp = data.pressure / Math.pow(1.0 - altitude/44330.0, 5.255);
+
+      console.log("Temp A: "+readout.temperature.toFixed(1)+"C, "+
+                  "Humidity: "+readout.humidity.toFixed(1)+"%, "+
+                  "Temp B: "+data.temperature.toFixed(1)+"C, "+
+                  "MSLP: "+mslp.toFixed(1)+" mbar");
+    });
+  }
+}
+
+station.start();
